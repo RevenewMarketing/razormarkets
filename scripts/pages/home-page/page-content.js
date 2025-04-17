c129178fd193cb76b6673639a35b9690e68627a4
Jason <jasonmortadev@gmail.com>
2025-04-17 10:16:38 +0200

    updates

--- scripts/pages/home-page/page-content.js
+++ scripts/pages/home-page/page-content.js
@@ -19,7 +19,7 @@ const section3 = [
   {
     title: 'Tailored Accounts',
     description: 'A variety of flexible account options crafted to suit and complement your unique trading style and needs.',
-     image: '../../../assets/home-page/Tailored-accounts.jpg'
+     image: '../../../assets/home-page/Advanced-tools.jpg'
   },
   // Add more card objects as needed
 ]
