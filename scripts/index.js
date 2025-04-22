b3ac4c643eb30a2a61224d286e8e9b5495469e64
JasonCodeWaves <jasonmortadev@gmail.com>
2025-04-22 10:13:31 +0200

    updated nav logic

--- scripts/index.js
+++ scripts/index.js
@@ -20,26 +20,6 @@ function backToTopButton() {
 
   window.addEventListener('scroll', toggleBackToTopButton);
 
-
-  // Select all elements with the class 'navbar-burger'
-  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if (navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    navbarBurgers.forEach(burger => {
-      burger.addEventListener('click', () => {
-        // Get the target from the 'data-target' attribute
-        const target = burger.dataset.target;
-        const targetElement = document.getElementById(target);
-
-        // Toggle the 'is-active' class on both the navbar-burger and the navbar-menu
-        burger.classList.toggle('is-active');
-        targetElement.classList.toggle('is-active');
-      });
-    });
-  }
-
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -75,7 +55,7 @@ function disclaimer() {
 backToTopButton()
 disclaimer()
 
-
+// changes nav background color on scroll
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('.navbar');
   const dropdown = document.querySelectorAll('.navbar-dropdown');
