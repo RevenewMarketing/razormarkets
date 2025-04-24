1e3a0083fd838b183db3416fff66203d057daf74
JasonCodeWaves <jasonmortadev@gmail.com>
2025-04-24 14:17:04 +0200

    added modal

---
+++ components/modal/script.js
@@ -0,0 +1,49 @@
+const modalEL = `
+<div class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <p class="image is-4by3">
+      <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
+    </p>
+  </div>
+  <button class="modal-close is-large" aria-label="close"></button>
+</div>
+`
+
+
+
+// append modal to index body
+document.querySelector('body').insertAdjacentHTML('beforeend', modalEL)
+
+
+// open modal function
+function openModal() {
+
+  // check if modal is already open, if true, return
+  if (sessionStorage.getItem('modalOpen') === 'true') {
+    return;
+  }
+
+    // save modal open state to session storage
+    sessionStorage.setItem('modalOpen', 'true');
+
+
+  // activate modal
+  document.querySelector('.modal').classList.add('is-active');
+
+  // Optional: close modal on close button click
+  document.querySelector('.modal-close').addEventListener('click', () => {
+    document.querySelector('.modal').classList.remove('is-active');
+  });
+
+  // Optional: close modal when clicking on the background
+  document.querySelector('.modal-background').addEventListener('click', () => {
+    document.querySelector('.modal').classList.remove('is-active');
+  });
+}
+
+
+
+setTimeout(() => {
+  openModal()
+}, 1000)
