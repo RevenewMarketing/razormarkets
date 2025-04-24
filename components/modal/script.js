4b8673b04a151d306de6e4cc16b3467f8f676dbd
Nyashadzashe Ndhlovu <nynyashadzashe@gmail.com>
2025-04-24 16:58:01 +0200

    updated modal

--- components/modal/script.js
+++ components/modal/script.js
@@ -1,52 +1,52 @@
-const image = './ImportantNotice.png';
+const image = '../assets/about-page/Untitled-2.jpg';
 
 // path to image file
 const modalEL = `
 <div class="modal">
   <div class="modal-background"></div>
   <div class="modal-content">
-    <p class="image is-4by3">
+    <figure class="image is-4by3">
       <img src="${image}" alt="important notice">
-    </p>
+      <button class="modal-close is-large is-overlay" aria-label="close"></button>
+    </figure>
   </div>
-  <button class="modal-close is-large" aria-label="close"></button>
 </div>
-`
-
-
+`;
 
 // append modal to index body
-document.querySelector('body').insertAdjacentHTML('beforeend', modalEL)
-
+document.querySelector('body').insertAdjacentHTML('beforeend', modalEL);
 
 // open modal function
 function openModal() {
 
-  // check if modal is already open, if true, return
-  // if (sessionStorage.getItem('modalOpen') === 'true') {
-  //   return;
-  // }
+    // check if modal is already open, if true, return
+  if (sessionStorage.getItem('modalOpen') === 'true') {
+    return;
+  }
 
     // save modal open state to session storage
     sessionStorage.setItem('modalOpen', 'true');
-
-
   // activate modal
   document.querySelector('.modal').classList.add('is-active');
 
   // Optional: close modal on close button click
-  document.querySelector('.modal-close').addEventListener('click', () => {
-    document.querySelector('.modal').classList.remove('is-active');
-  });
+  const closeModalButton = document.querySelector('.modal-close');
+  if (closeModalButton) {
+    closeModalButton.addEventListener('click', () => {
+      document.querySelector('.modal').classList.remove('is-active');
+    });
+  }
 
   // Optional: close modal when clicking on the background
-  document.querySelector('.modal-background').addEventListener('click', () => {
-    document.querySelector('.modal').classList.remove('is-active');
-  });
+  const modalBackground = document.querySelector('.modal-background');
+  if (modalBackground) {
+    modalBackground.addEventListener('click', () => {
+      document.querySelector('.modal').classList.remove('is-active');
+    });
+  }
 }
 
-
-
+// Open the modal after a short delay (adjust as needed)
 setTimeout(() => {
-  openModal()
-}, 1000)
+  openModal();
+}, 1000);
